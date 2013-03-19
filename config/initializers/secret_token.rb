39853aa88a5b53c8c676836fb494b073ac7c1863
Renee <renee@renee-Inspiron-1545.(none)>
2013-03-19 16:49:31 -0700

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Brainstormy::Application.config.secret_token = '7177cde30553abf7ce2b1c3fa583825a30fc35ef25fd958ca561f8d2f94a98b38af77dc73df6238f536f2e8ff022b68ddefece11b772a5067cfb49feb73c27d7'
