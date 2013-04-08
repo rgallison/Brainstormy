9be76e62b0a032808f52dd311568f2f613dd2fdd
Renee Gallison <reneeggallison@gmail.com>
2013-04-08 15:36:59 -0700

    got the login feature working!  Still need to add flash and input validation.  Also updated css and rearranged home view.  Added to application controller and corresponding routes.  Added a logout option

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,8 +3,19 @@ class ApplicationController < ActionController::Base
 
   before_filter :set_current_user
 
+  def create_session
+  	session[:user] = User.where(:username => params[:user][:username_login], :password => params[:user][:password_login])
+  	set_current_user
+  	redirect_to root_path
+  end
+
   def set_current_user
   	@current_user ||= session[:user]
   end
 
+  def logout_user
+  	reset_session
+  	@current_user = nil
+  	redirect_to root_path
+  end
 end
