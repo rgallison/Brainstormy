f5944b05854b9e9bd523582d91fa68b4aaf6a46b
Renee Gallison <reneeggallison@gmail.com>
2013-04-10 12:32:02 -0700

    a bit more css

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,10 +6,11 @@ class ApplicationController < ActionController::Base
   def create_session
   	#gets params and finds user with such a username and password, then sets user to session -rg
   	user = User.find_by_username(params[:user][:username_login])
-    if user.password == params[:user][:password_login] and (session[:user_id] = user.id if user)
+  	if (session[:user_id] = user.id if user)
   		set_current_user#sets sets @current_user if user was found -rg
+  	else
+  		flash[:warning] = "That is not a valid login.  Please try again."#setting falsh warning if no such user exists -rg
   	end
-    flash[:warning] = "That is not a valid login.  Please try again."#setting falsh warning if no such user exists -rg
   	redirect_to root_path#and to the home page! -rg
   end
 
