1400ec961063b338c996f5456c4ef83b4b3f5b94
Lindsey <lstratton@mills.edu>
2013-04-26 20:02:50 -0700

    rspec tests for comment ranking. Currently pending because upvoting and downvoting comments is implemented yet

---
+++ spec/controllers/comments_controller_spec.rb
@@ -0,0 +1,14 @@
+require 'spec_helper'
+
+describe CommentsController do
+  describe 'PENDING: vote on comment' do
+    it 'PENDING: should call the method that changes rank' do
+    end#call the thing that changes rank
+    it 'PENDING: should decrease rank by one' do
+    end#downvote
+    it 'PENDING: should increase rank by one' do
+    end#upvote
+    it 'PENDING: resort view' do
+    end#resort
+  end#describe upvoting and downvoting
+end#describe Comments Controller
