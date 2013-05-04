8cd62ce3cc98ea32571c91d0f0e3b29aba070ee6
Lindsey <lstratton@mills.edu>
2013-05-03 17:20:09 -0700

    comments are now votable

--- config/routes.rb
+++ config/routes.rb
@@ -9,6 +9,9 @@ Brainstormy::Application.routes.draw do
 
   match '/application/create_session' => 'application#create_session'
   match '/application/logout_user' => 'application#logout_user'
+
+	match '/comments/upvote' => 'comments#upvote'
+	match '/comments/downvote' => 'comments#downvote'
   # get '/messages/:id' => 'messages#index', :as => :message
   # get '/messages' => 'messages#index', :as => :messages
   # get '/messages/new' => 'messages#create', :as => :new_message
