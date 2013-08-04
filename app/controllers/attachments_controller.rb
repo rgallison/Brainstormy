b34ce264a21fe8531679fcf6302e05df5561b37e
Renee Gallison <reneeggallison@gmail.com>
2013-08-04 15:48:41 -0700

    Got uploader working!  For images, with validation.  Now to try and get pdfs...

--- app/controllers/attachments_controller.rb
+++ app/controllers/attachments_controller.rb
@@ -1,8 +1,7 @@
 class AttachmentsController < ApplicationController
 	def create
-		# raise params[:attachment].inspect
-		@attachment = Attachment.new(params[:attachment])
-		Idea.find_by_id(params[:attachment][:idea_id]).owner_attachments << @attachment
+		@attachment = Attachment.new(params[:attachment]) #create attachment object
+		Idea.find_by_id(params[:attachment][:idea_id]).owner_attachments << @attachment #add attachment object to association
 		if @attachment.save
 			flash[:notice] = "File has been successfully uploaded."
 		else
@@ -12,6 +11,15 @@ class AttachmentsController < ApplicationController
 			end
 		end
 		redirect_to edit_idea_path(@attachment.idea_id)
+	end
 
+	def destroy
+		# raise params.inspect
+		@attachment = Attachment.find_by_id(params[:id]) #get the attachment object
+		@idea = Idea.find_by_id(@attachment.idea_id) #get the idea that parents it
+		@idea.owner_attachments.find(params[:id]).attachment = nil #Removes the attachment from storage
+		@idea.owner_attachments.destroy(@attachment) #remove the attachment object from association
+		@attachment.destroy #destroy attachment object
+		redirect_to edit_idea_path(@idea)
 	end
 end
--- config/environment.rb
+++ config/environment.rb
@@ -4,3 +4,5 @@ require File.expand_path('../application', __FILE__)
 # Initialize the rails application
 Brainstormy::Application.initialize!
 
+Paperclip.options[:command_path] = "/usr/bin/"
+
